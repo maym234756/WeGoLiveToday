1525871989fbed726b12ca961bd38d27f756d45f
maym23475 <maym23475@yahoo.com>
2025-12-07 15:00:42 -0600

    Re-push everything

--- apps/web/app/coming-soon/page.tsx
+++ apps/web/app/coming-soon/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import TrackView from '@/components/Trackview';
+import Trackview from '@/components/Trackview';
 
 export default function ComingSoon() {
   const router = useRouter();
