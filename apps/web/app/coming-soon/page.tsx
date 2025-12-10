9cebba6dcf060702516d09eab459013e69fde5e2
maym23475 <maym23475@yahoo.com>
2025-12-09 20:31:16 -0600

    Re-push everything

--- apps/web/app/coming-soon/page.tsx
+++ apps/web/app/coming-soon/page.tsx
@@ -153,7 +153,7 @@ export default function ComingSoon() {
             <span className="font-semibold text-white">You're in!</span> Once subscribed, an arrow ➡️ will appear giving you access to future updates.
           </li>
           <li>
-            <span className="font-semibold text-white"></span> Add an email if you'd like ewmail notification on new release pages.
+            <span className="font-semibold text-white"></span> Add an email if you'd like email notification on new release pages.
           </li>
         </ol>
 
